2050b4b7760cc11cbeabe9c4d981c618d0116587
Denimar Fernandez <fdenimar@hotmail.com>
2017-01-10 05:57:48 +0800

    Integrate agaiiin

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Backpack\CRUD\CrudServiceProvider::class,
-        Backpack\PermissionManager\PermissionManagerServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
@@ -180,6 +179,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Backpack\PermissionManager\PermissionManagerServiceProvider::class,
 
     ],
 
